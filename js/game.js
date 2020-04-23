ca285baefcc4fe6b61735db9d75e3ddd4000cbd5
Adithya Mallya <55098608+adiMallya@users.noreply.github.com>
2020-04-23 21:37:01 +0530

    updated code

--- js/game.js
+++ js/game.js
@@ -1,1 +1,75 @@
-function play(){}
+function play(){
+    let pscore =0;
+    let cscore =0;
+     //fill these queries
+    const option = document.querySelector();//player chooses
+    const phand = document.querySelector();
+    const chand = document.querySelector();
+
+    const compOpts =['rock','paper','scissors'];
+    
+    //this part is incomplete
+    const compNum = Math.floor(Math.random()*3+1);
+    const compChoice = compOpts[compNum];
+      
+    compareChoice(playerChoice,compChoice);
+    //    
+    function scoreUpd(){
+        const playerScore = document.getElementById("player1");
+        const compScore = document.getElementById("player2");
+        playerScore.innerHTML = pscore;
+        compScore.innerHTML = cscore;
+    }
+    function compareChoice(playerChoice,compChoice){
+        const winner = document.getElementById();//paragraph to display result
+        if(playerChoice===compChoice){
+           return ;
+           write.innerHTML = "It is a Tie!";
+        }
+
+        if (playerChoice=='rock')
+        {    
+            if(compareChoice =='scissors'){
+             write.innerHTML = "Player Win";
+             pscore++;
+             scoreUpd();
+             return;
+            }
+            else {
+                write.innerHTML = "Comp Win";
+                cscore++;
+                scoreUpd();
+                return;
+            }
+        }
+        if (playerChoice =='paper'){
+            if(compChoice=='scissors'){
+              write.innerHTML = "Comp Win";
+                cscore++;
+                scoreUpd();
+                return;
+            }
+            else{
+                write.innerHTML = "Player Win";
+                pscore++;
+                scoreUpd();
+                return;
+            }
+        }
+        if(playerChoice=='scissors'){
+            if(compChoice=='rock'){
+               write.innerHTML = "Comp Win";
+                cscore++;
+                scoreUpd();
+                return;
+            }
+            else{
+                write.innerHTML = "Player Win";
+                pscore++;
+                scoreUpd();
+                return;
+            }
+        }
+     }  
+
+}
